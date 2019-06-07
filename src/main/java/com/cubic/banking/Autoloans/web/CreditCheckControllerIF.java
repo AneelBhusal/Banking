cd077abeca5269000a8cbb6411f2483ddd7cdac5
Prakas <prakas702@gmail.com>
2019-06-07 13:07:07 -0500

    Added interface for CreditController

---
+++ src/main/java/com/cubic/banking/Autoloans/web/CreditCheckControllerIF.java
@@ -0,0 +1,8 @@
+package com.cubic.banking.Autoloans.web;
+import com.cubic.banking.Autoloans.DTO.ApplicationDTO;
+
+
+public interface CreditCheckControllerIF {
+
+		public String checkCredit(ApplicationDTO dto);
+}
